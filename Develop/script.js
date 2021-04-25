257362a6a6d9d0f6bcea3480d028b5d63f081a52
Eric Webber <ewebber283@gmail.com>
2021-04-25 13:34:52 -0500

    correctly formatted blocks

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,15 @@
 
+// Add ability to click into form and create then save tasks
+
+// add ability to show date
+function getDate() {
+    var todaysDate = document.getElementById("currentDay");
+    todaysDate.innerHTML = moment();
+}
+
+getDate();
+
+// Add changing colors depending on where time block is 
 
 /* var saveTasks = function() {
     localStorage.setItem("tasks", JSON.stringify(tasks));
