e04267a5962860a668f8b65fa915ae53c350d34e
Eric Webber <ewebber283@gmail.com>
2021-04-24 19:35:03 -0500

    added input forms for each time

---
+++ Develop/script.js
@@ -0,0 +1,14 @@
+
+
+/* var saveTasks = function() {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+var loadTasks = function() {
+    tasks = JSON.parse(localStorage.getItem("tasks"));
+
+    if(!tasks) {
+        tasks = "";
+    }
+} */
+
